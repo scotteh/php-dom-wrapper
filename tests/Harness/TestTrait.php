ef5424be0d154ab1be46b656c013b9ac93526cee
Andrew <andrew@andrewscott.net.au>
2015-05-06 00:15:36 +1000

    Commit travis / phpunit config and test harness.

---
+++ tests/Harness/TestTrait.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace DOMWrap\Tests\Harness;
+
+use DOMWrap\Element;
+use DOMWrap\Document;
+
+/**
+ * Test Trait
+ *
+ * @package DOMWrap\Tests\Harness
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD 3 Clause
+ */
+trait TestTrait {
+    /**
+     * @param Document|Element $doc
+     *
+     * @return string
+     */
+    public function html($doc) {
+        if ($doc instanceof Document) {
+            $el = $doc->documentElement;
+        } else if ($doc instanceof Element) {
+            $el = $doc;
+            $doc = $doc->ownerDocument;
+        }
+
+        return $doc->saveXML($el);
+    }
+
+    /**
+     * @param string $html
+     *
+     * @return Document
+     */
+    private function document($html) {
+        $doc = new Document();
+        $doc->html($html);
+
+        // Remove the doctype (if it exists) so we can use \DOMDocument::$firstChild
+        if ($doc->doctype instanceof \DOMDocumentType) {
+            $doc->removeChild($doc->doctype);
+        }
+
+        return $doc;
+    }
+
+    /**
+     * @param string $method
+     * @param mixed $arguments,...
+     *
+     * @return mixed
+     */
+    private function call($method) {
+        $arguments = func_get_args();
+
+        array_shift($arguments);
+
+        $obj = new self::$CLASS_NAME($this->config());
+
+        $class = new \ReflectionClass(self::$CLASS_NAME);
+
+        if (!$class->hasMethod($method)) {
+            throw new BadMethodCallException();
+        }
+
+        $fn = $class->getMethod($method);
+        $fn->setAccessible(true);
+        $result = $fn->invokeArgs($obj, $arguments);
+
+        return $result;
+    }
+}
